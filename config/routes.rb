9bb337b64be989274e3a6e0ee71c71769e75b7f4
nupurb <nupurb@gmail.com>
2017-09-15 14:10:54 -0700

    Added starting point for ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'games/new'
   get 'games/play'
   devise_for :users
+  mount ActionCable.server => "/cable"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
