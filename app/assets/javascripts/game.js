122cdc91022db28f30e6486e130b251972d21c98
casejaz <jizhu@g.hmc.edu>
2017-09-23 13:36:06 -0700

    Add cards to game.js

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -2,8 +2,21 @@ var Game = function(element, playerId) {
   console.log("GAME CREATED", playerId)
   this.element = $(element); // This element is game-container. We can put stuff there!
   this.init = function() {
-    this.center_deck = [[1,'a'],[1, 'b']]
-    this.remaining_deck = []
+    this.center_deck = [];
+    this.remaining_deck = [];
+    this.players = [];
+    this.ranks = [1,1,1,2,2,3,3,4,4,5];
+    this.suites = ['A','B','C','D','E'];
+
+    //Create the initial deck
+    for (i = 0; i < this.ranks.length; i++) {
+      for (j = 0; j < this.suites.length; j++){
+        this.remaining_deck.push([this.ranks[i],this.ranks[j]]);
+      }
+    }
+    //
+    // //Create the hands of players
+    // this.distributeCards();
   };
   this.start = function() {
     this.init()
@@ -12,19 +25,24 @@ var Game = function(element, playerId) {
     // similar to the game.js file in the tic-tac-toe tutorial.
     console.log("We started!");
     console.log("player", playerId);
+    console.log(this.remaining_deck.length);
 
-
-    if (playerId == 1) {
-      $('#card').html('1a');
-      console.log(this.center_deck[0]);
-    } else {
-      $('#card').html('1b');
-      console.log(this.center_deck[1]);
-    }
+    $('#card').html(this.suites[playerId]);
     console.log("test jquery")
     $('#status').html('Your turn');
   }
 
+  // this.distributeCards = function() {
+  //   for (i = 0; i < this.players.length; i++) {
+  //     var x = 0;
+  //     while (x < 5) {
+  //       var index = Math.floor(Math.random() * this.remaining_deck.length);
+  //       this.players[i].push(this.remaining_deck[index]);
+  //       this.remaining_deck.splice(index,1);
+  //       x += 1;
+  //     }
+  //   };
+  // };
 
 
   this.start();
